e8eb9f4689921914372ae1053af9541d811b89f7
Md. Abdul Awal Sazib <sazib1207@gmail.com>
2022-04-21 02:21:41 +0600

    New functionalities added

--- src/main/java/com/example/SimpleMath.java
+++ src/main/java/com/example/SimpleMath.java
@@ -10,10 +10,17 @@ public class SimpleMath {
     }
 
     public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Error:: Divison cannot be 0");
+        }
         return a / b;
     }
 
     public long multiply(int a, int b) {
         return a * b;
+    }
+
+    public int remainder(int a, int b) {
+        return a % b;
     }
 }
---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example","l":"AppLauncher"},{"p":"com.example","l":"SimpleMath"}]
